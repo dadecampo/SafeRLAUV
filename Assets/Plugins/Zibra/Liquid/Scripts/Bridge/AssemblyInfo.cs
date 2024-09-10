98d95e6a2003824f25d6a6f1969c5dcf837367b5
Davide Camponogara <davide.camponogara.99@gmail.com>
2024-09-10 18:28:56 +0200

    remove plugins and python package

--- Assets/Plugins/Zibra/Liquid/Scripts/Bridge/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("ZibraAI.ZibraEffects.Liquid")]
-[assembly:InternalsVisibleTo("ZibraAI.ZibraEffects")]
-[assembly:InternalsVisibleTo("ZibraAI.ZibraEffects.Editor")]
--- Python/aquatic_navigation_gym/uuv_envs/__init__.py
+++
@@ -1,3 +0,0 @@
-from aquatic_navigation_gym.uuv_envs.envs import EasyUuvEnv, MediumUuvEnv, HardUuvEnv, UuvEnv
-
-
